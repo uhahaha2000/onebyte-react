bb2548af28e66a6c1682c514740063bfc494f362
uhahaha2000 <uhahaha2000@naver.com>
2025-03-23 20:38:18 +0900

    Edit 기능 추가

--- section12/src/components/Editor.jsx
+++ section12/src/components/Editor.jsx
@@ -2,7 +2,7 @@ import Button from "./Button";
 import "./Editor.css";
 import EmotionItem from "./EmotionItem";
 import { useNavigate } from 'react-router-dom';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const emotionList = [
   {
@@ -42,7 +42,7 @@ const getStringedDate = (targetDate) => {
   return `${year}-${month}-${date}`;
 };
 
-const Editor = ({ onSubmit }) => {
+const Editor = ({ initData, onSubmit }) => {
   const [input, setInput] = useState({
     createdDate: new Date(),
     emotionId: 3,
@@ -65,9 +65,17 @@ const Editor = ({ onSubmit }) => {
 
   const nav = useNavigate();
 
+  useEffect(() => {
+    if (initData) {
+      setInput({
+        ...initData,
+        createdDate: new Date(Number(initData.createdDate)),
+      });
+    }
+  }, [initData]);
+
   const onClickSubmitButton = () => {
     onSubmit(input);
-    nav(-1);
   };
 
   return (
--- section12/src/pages/Edit.jsx
+++ section12/src/pages/Edit.jsx
@@ -1,7 +1,68 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
+import Header from '../components/Header';
+import Button from '../components/Button';
+import Editor from '../components/Editor';
+
+import { useContext, useEffect, useState } from 'react';
+import { DiaryDispatchContext, DiaryStateContext } from '../App';
 
 const Edit = () => {
   const params = useParams();
-  return <div>{ params.id}번 일기를 수정하자</div>;
+  const nav = useNavigate();
+  const { onDelete, onUpdate } = useContext(DiaryDispatchContext);
+  const data = useContext(DiaryStateContext); // 전체 데이터 불러왔다!
+  const [curDiaryItem, setCurDiaryItem] = useState();
+
+  useEffect(() => {
+    const currentDiaryItem = data.find(
+      (item) => String(item.id) === String(params.id)
+    );
+
+    if (!currentDiaryItem) {
+      window.alert('존재하지 않는 일기입니다.');
+      nav('/', { replace: true });
+    }
+
+    setCurDiaryItem(currentDiaryItem);
+  }, [params.id]);
+
+  const onClickDelete = () => {
+    if (window.confirm('정말 삭제하시겠습니까?')) {
+      // 삭제 로직
+      onDelete(params.id);
+      nav('/', { replace: true });
+    }
+  };
+
+  const onSubmit = (input) => {
+    if (window.confirm('수정하시겠습니까?')) {
+      onUpdate(
+        params.id,
+        input.createdDate.getTime(),
+        input.emotionId,
+        input.content
+      );
+      nav('/', { replace: true });
+    }
+  };
+  return (
+    <>
+      <Header
+        title='일기 수정하기'
+        leftChild={
+          <Button
+            text={'< 뒤로가기'}
+            onClick={() => {
+              nav(-1);
+            }}
+          />
+        }
+        rightChild={
+          <Button text={'삭제하기'} type='negative' onClick={onClickDelete} />
+        }
+      />
+      <Editor initData={curDiaryItem} onSubmit={onSubmit} />
+    </>
+  );
 };
 export default Edit;
