6eb0a28280d1e252876ce537b6d910751508da87
uhahaha2000 <uhahaha2000@naver.com>
2025-04-13 22:09:24 +0900

    todo 추가 수정 삭제 체크박스

--- section08-again/src/components/TodoListItem.jsx
+++ section08-again/src/components/TodoListItem.jsx
@@ -1,18 +1,70 @@
+import { useState } from 'react';
 import './TodoListItem.css';
 
-function TodoListItem() {
+function TodoListItem({ id, content, date, done, onUpdate, onRemove }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editContent, setEditContent] = useState(content);
+
+  const onChangeCheckbox = () => {
+    onUpdate(id);
+  };
+
+  const onClickDelete = () => {
+    onRemove(id);
+  };
+
+  const onClickModify = () => {
+    setIsEditing(true);
+  };
+
+  const onSaveEdit = () => {
+    onUpdate(id, editContent);
+    setIsEditing(false);
+  };
+
+  const onCancelEdit = () => {
+    setEditContent(content);
+    setIsEditing(false);
+  };
+
   return (
     <>
       <div className='todo-list-item'>
-        <input type='checkbox' id='todo1' />
-        <label className='todo_text' htmlFor='todo1'>
-          <span>할 일</span>
-        </label>
-
-        <span className='date'>2025.4.13</span>
-        <button type='button' className='btn_delete'>
-          삭제
-        </button>
+        <input
+          type='checkbox'
+          id={id}
+          checked={done}
+          onChange={onChangeCheckbox}
+        />
+        {isEditing ? (
+          <>
+            <input
+              type='text'
+              value={editContent}
+              onChange={(e) => setEditContent(e.target.value)}
+              className='edit-input'
+            />
+            <button type='button' className='btn_save' onClick={onSaveEdit}>
+              저장
+            </button>
+            <button type='button' className='btn_cancel' onClick={onCancelEdit}>
+              취소
+            </button>
+          </>
+        ) : (
+          <>
+            <label className='todo_text' htmlFor={id}>
+              {content}
+            </label>
+            <span className='date'>{new Date(date).toLocaleDateString()}</span>
+            <button type='button' className='btn_modify' onClick={onClickModify}>
+              수정
+            </button>
+            <button type='button' className='btn_delete' onClick={onClickDelete}>
+              삭제
+            </button>
+          </>
+        )}
       </div>
     </>
   );
